724b16b002159a1ad5564e2119dccc411041961f
Alberto Taddei <albertotaddei1@gmail.com>
2026-02-23 20:37:07 +0100

    class definition

---
+++ src/DiffusionNonLinear.hpp
@@ -0,0 +1,41 @@
+#ifndef DIFFUSION_NON_LINEAR_HPP
+#define DIFFUSION_NON_LINEAR_HPP
+
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_vector.h>
+
+#include "parameters.hpp"
+
+using namespace dealii;
+
+template <int dim>
+class DiffusionNonLinear {
+public:
+    DiffusionNonLinear(const SimulationParameters &params);
+    void run();
+
+private:
+    void setup_system();
+    void assemble_system();
+    void solve_time_step();
+    void output_results(const unsigned int time_step) const;
+
+    SimulationParameters params;
+
+    parallel::distributed::Triangulation<dim> triangulation;
+    FE_Q<dim>       fe;
+    DoFHandler<dim> dof_handler;
+
+    TrilinosWrappers::SparseMatrix system_matrix;
+    TrilinosWrappers::MPI::Vector  solution;
+    TrilinosWrappers::MPI::Vector  old_solution;
+    TrilinosWrappers::MPI::Vector  system_rhs;
+
+    ConditionalOStream pcout;
+};
+
+#endif
